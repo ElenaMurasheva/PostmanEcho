459443bea0a15c8d04509233d17921643f3a97cc
Елена Мурашева <vacilika77@gmail.com>
2022-08-10 11:55:47 +0300

    Added RAEDME

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostMessage() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hello world") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hello world"))
+        ;
+    }
+
+
+}
